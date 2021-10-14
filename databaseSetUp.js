fced02cf60b6eaca37f7d7b58be297658a27906a
JackBurns136 <57546815+JackBurns136@users.noreply.github.com>
2021-10-14 18:34:10 -0400

    second commit

--- databaseSetUp.js
+++ databaseSetUp.js
@@ -2,21 +2,21 @@ const { Console } = require('console');
 
 const sqlite3 = require('sqlite3').verbose();
 //Opens a database in memory
-let db = new sqlite3.Database('C:\\Users\\JackB\\Desktop\\QU2122-Fall\\CSC-375\\SQLite\\GameDatabase.db', (error) => {
+let db = new sqlite3.Database('C:\\Users\\JackB\\Desktop\\QU2122-Fall\\CSC-375\\SQLite\\GameDatabase.db', (error) => {// could potentially change the long sequence to __dirname
     if(error){
         return console.log(error.message);
     }
     console.log('Connection Successful');
 });
-/*
+
 db.run(`INSERT INTO Photos(photoID) VALUES(?)`, [6],function(error) {
     if(error) {
         console.log(error.message);
     }
     console.log(this.lastID);
 });
-*/
-db.run(`DELETE FROM Photos WHERE rowid=?`,[3]);
+
+
 
 //closes the database
 db.close((error) => {
