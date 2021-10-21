2287bebc0199ee31c47ad9aae3e7828a9a2ea9d9
JackBurns136 <57546815+JackBurns136@users.noreply.github.com>
2021-10-21 15:48:08 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Repo Title
+# CSC-375
 
 ## What is this project?
 
 This project is an open-source database that can be used to store images.  
+I am using Node.js as the runtime environment and obviously JavaScript to code the project.
+SQLite is the database program used to hold the images.
+
+## Agenda
+    Write a function to better write the database to a website
+    Find the best way to have images in a database (store images outside of the database with a link to the image stored in the DB)
--- databaseSetUp.js
+++ databaseSetUp.js
@@ -8,14 +8,25 @@ let db = new sqlite3.Database('C:\\Users\\JackB\\Desktop\\QU2122-Fall\\CSC-375\\
     }
     console.log('Connection Successful');
 });
-
+/*
 db.run(`INSERT INTO Photos(photoID) VALUES(?)`, [6],function(error) {
     if(error) {
         console.log(error.message);
     }
     console.log(this.lastID);
 });
-
+*/
+let sql = 'SELECT * FROM Photos';
+//db.all returns all rows
+//db.get returns only 1 row
+db.all(sql,[], (err, rows) => {
+    if(err) {
+        throw err;
+    }
+    rows.forEach((row) => {
+        console.log(row.name);
+    })
+});
 
 
 //closes the database
