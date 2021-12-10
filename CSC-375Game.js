71537e0209bae89f152a0cd0e4022ae0659e8688
JackBurns136 <57546815+JackBurns136@users.noreply.github.com>
2021-12-09 19:30:44 -0500

    added the Database to git, added more photos, started implementation on response back to user input

--- CSC-375Game.js
+++ CSC-375Game.js
@@ -1,64 +1,17 @@
-const http = require('http');
 const host = '127.0.0.1';
 const port = 8000;
 const cors = require('cors')
 const express = require('express');
 const app = express();
 const imageToBase64 = require('image-to-base64');
+const sqlite3 = require('sqlite3').verbose();
 app.use(cors());
 
 
-// const requestListener = function (req,res) {
-//     const sqlite3 = require('sqlite3').verbose();
-//     //Opens a database in memory
-//     let db = new sqlite3.Database('C:\\Users\\JackB\\Desktop\\QU2122-Fall\\CSC-375\\SQLite\\GameDatabase.db', (error) => {// could potentially change the long sequence to __dirname
-//         if(error){
-//             return console.log(error.message);
-//         }
-//         console.log('Connection Successful');
-//     });
-
-//     let sql = 'SELECT * FROM Photos';
-//     db.all(sql,[], (err, rows) => {
-//         if(err) {
-//             throw err;
-//         }
-//         rows.forEach((row) => {
-//             let buff = new Buffer(row.PhotoFilePath);
-//             let base64 = buff.toString('base64');
-//             res.end(row.PhotoID + " " + base64);
-//         })
-//     });
-
-//     //closes the database
-// // db.close((error) => {
-// //     if(error){
-// //         console.log(error.message);
-// //     }
-// //     console.log('Database Closed');
-// // });
-
-// res.writeHead(200);
-// };
-// const server = http.createServer(requestListener);
-// server.listen(port,host, () => {
-// console.log(`Server is running on http://${host}:${port}`);
-// });
-
-function streamToString (stream) {
-    const chunks = [];
-    return new Promise((resolve, reject) => {
-      stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
-      stream.on('error', (err) => reject(err));
-      stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
-    })
-  }
-
 app.get('/', function(req,res) {
     
-    const sqlite3 = require('sqlite3').verbose();
     //Opens a database in memory
-    let db = new sqlite3.Database('C:\\Users\\JackB\\Desktop\\QU2122-Fall\\CSC-375\\SQLite\\GameDatabase.db', (error) => {// could potentially change the long sequence to __dirname
+    let db = new sqlite3.Database('C:\\Users\\JackB\\Desktop\\QU2122-Fall\\CSC-375\\JavaScriptProjects\\SQLite\\GameDatabase.db', (error) => {// could potentially change the long sequence to __dirname
         if(error){
          console.log(error.message);
         }
@@ -70,29 +23,72 @@ app.get('/', function(req,res) {
         if(err) {
             throw err;
         }
-        imageToBase64(rows[0].PhotoFilePath)
+        
+         //Fills an array with the images, have to update the if statement to the size of the database
+        let celebList = [];
+        for(let i = 0; i < 13; i++){
+            console.log(celebList[i] = rows[i].PhotoFilePath);
+        };
+        
+        //Random number genetated then sent to the front end as Base64
+        let rand = Math.floor(Math.random() * 13)
+        console.log(rows[rand].CelebrityName)
+        imageToBase64(celebList[rand])
             .then(response => {
-                res.send(response);
-                
+                res.send(JSON.stringify({body: response}));
             })
-
             .catch(error => {
                 console.log(error);
             })
-        // let buff = new Buffer(row.PhotoFilePath);
-        // let base64 = buff.toString('base64');
-        // res.send(base64);
-            
     });
-
-    //closes the database
-db.close((error) => {
-    if(error){
-        console.log(error.message);
-    }
-    console.log('Database Closed');
-});
     
+    //closes the database
+    db.close((error) => {
+        if(error){
+        console.log(error.message);
+        }
+     console.log('Database Closed');
+    })
+    
+})
+
+app.post('/checkAnswer', function (req,res) {
+
+    let db2 = new sqlite3.Database('C:\\Users\\JackB\\Desktop\\QU2122-Fall\\CSC-375\\SQLite\\GameDatabase.db', (error) => {
+        if(error){
+         console.log(error.message);
+        }
+        console.log('Connection Successful');
+    });
+    
+    const body = req.body
+    const imageName = body.imageName // correct answer from the database
+    const answer = body.answer //answer user inputted
+    let sqlAnswer = 'SELECT * FROM Photos';
+    console.log(body);
+    console.log(imageName);
+    console.log(answer);
+    console.log(sqlAnswer);
+
+    db2.all(sqlAnswer, [], (err, rows) => {
+        if(err) {
+            throw err;
+        }
+        for(let i = 0; i < 13; i++) {
+            if(answer === imageName) {
+                res.send("Correct");
+            } else {
+                res.send("Incorrect");
+            }
+        }
+    })
+
+    db2.close((error) => {
+        if(error){
+        console.log(error.message);
+        }
+     console.log('Database Closed');
+    })
 })
 
 const server = app.listen(8081, function (){
